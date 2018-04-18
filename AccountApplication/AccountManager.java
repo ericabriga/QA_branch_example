9ec7b7fc7979e52e698a8655aba5914129452652
Erica Briga <erica.briga@gmail.com>
2018-04-18 14:22:06 +0100

    pushing first part of code

--- AccountApplication/AccountManager.java
+++ AccountApplication/AccountManager.java
@@ -3,13 +3,15 @@ package AccountApplication;
 public class AccountManager {
 
 	public static void main(String[] args) {
+		
 		Service service = new Service();
-		
-		service.addAccount("Jay","M", "123");
-		service.addAccount("John","T", "123");
-		service.addAccount("Max","R", "123");
+		//Account Jay = new Account("jay","m","123");
+		Account jay = new Account("Jay","M","123");
+		//service.addAccount();
+		service.addAccount(new Account("John","T", "456"));
+		service.addAccount(new Account("Max","R", "789"));
 
-		
+		System.out.println(service.getAccount(jay.getAccountNumber()));
 	}
 
 }
--- AccountApplication/Service.java
+++ AccountApplication/Service.java
@@ -3,14 +3,15 @@ package AccountApplication;
 import java.util.HashMap;
 
 public class Service {
-	
+	//static int count =1;
 	HashMap<String,Account> listAccount = new HashMap <String,Account> ();
 	
 	public boolean addAccount (Account account) {
-		if (account != null) {
+		Account exists = listAccount.get(account.getAccountNumber()); 
+		if (exists != null) {
 			return false;
 		} else {
-		listAccount.put(account.getAccountNumber(), account);
+			listAccount.put(account.getAccountNumber(), account);
 			return true;
 		}
 	}
@@ -21,12 +22,40 @@ public class Service {
 		Account account = listAccount.get(number);
 		if (account != null) {
 			return account;
+			//return account.getFirstName() + " " + account.getLastName() + " " + account.getAccountNumber();
 		} else {
 			return null;
 		}
 		
 		
 	}
+/*public void addAccount(String firstName, String lastName, String accountNumber) {
+		
+		Account account = new Account(firstName, lastName, accountNumber);
+		listAccount.put(count, account);
+		count++;
+		
+	}
 	
+	public void getAccount(Account account) {
+		account.toString();
+	}
+	
+	public void showMap() {
+        Set<Entry<Integer, Account>> hashSet=listAccount.entrySet();
+        for(Entry entry:hashSet ) {
+            System.out.println(entry.getKey()+ ", " + entry.getValue());
+        }
+	}
+	
+	public void showEntry(int num) {
+        Set<Entry<Integer, Account>> hashSet=listAccount.entrySet();
+        for(Entry entry:hashSet ) {
+        	int key = (Integer) entry.getKey();
+        	if(key==num) {
+        		System.out.println(entry.getKey()+ ", " + entry.getValue());
+        	}
+        }
+	}*/
 }
 
